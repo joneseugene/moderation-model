834806f894fc35beef86c61a9b3ccd6365895d6f
joneseugeneceo <joneseugeneceo@gmail.com>
2026-05-23 01:28:58 +0000

    Change run.py

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True,
-        reload_excludes=["*.log", "*.db", "__pycache__", ".git"]
+        host="0.0.0.0",
+        port=port,
+        reload=False
     )
